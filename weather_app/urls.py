0cb170d4bd9c1ac5a2be42a72163d7e92d2dd138
Rosana Shopova <rosanashopova@coherentsolutions.com>
2022-07-20 15:16:53 +0300

    Filter out urls
    Filter out only the needed urls from django-rest-auth

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path, include
 from weather_app.views import CityWeatherView
+from rest_auth import urls as rest_auth_urls
+from rest_auth.registration import urls as rest_auth_registration_urls
+
+
+rest_auth_filtered_urls = [
+    url for url in rest_auth_urls.urlpatterns if 
+        url.name == 'rest_login' or
+        url.name == 'rest_logout' or
+        url.name == 'rest_password_change' or
+        url.name == 'rest_user_details'
+]
+
+rest_register_url = [url for url in rest_auth_registration_urls.urlpatterns if url.name == 'rest_register']
 
 urlpatterns = [
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('auth/', include(rest_auth_filtered_urls)),
+    path('auth/registration/', include(rest_register_url)),
     path('check-by-city/<str:city>/', CityWeatherView.as_view()),
 ]
